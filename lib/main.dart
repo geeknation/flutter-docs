683fc34c63f54f7098af04f05e8a5d4dbb1686db
ian_mukunya2 <ianmu4046@gmail.com>
2023-03-18 07:38:38 +0000

    stateless widget - base app

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Flutter Docs App',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
