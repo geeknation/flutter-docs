56f03197e8a67967dd95a40c841292c7686819e4
ian_mukunya2 <ianmu4046@gmail.com>
2023-03-18 17:03:13 +0000

    Container widget example

--- lib/ContainerWidget.dart
+++ lib/ContainerWidget.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
 
-class ContainerWidget extends StatelessWidget {}
+class ContainerWidget extends StatelessWidget {
+  Widget build(BuildContext conx) {
+    return (Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(10.0),
+      width: 200.0,
+      height: 200.0,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10.0,
+            offset: Offset(5.0, 5.0),
+          ),
+        ],
+      ),
+      transform: Matrix4.rotationZ(0.1),
+      child: Text(
+        'Hello World',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+        ),
+      ),
+    ));
+  }
+}
