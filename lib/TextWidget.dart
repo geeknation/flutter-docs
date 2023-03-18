4b0455438a91fc8dbe753d50705acbb9840bc8e6
ian_mukunya2 <ianmu4046@gmail.com>
2023-03-18 07:52:31 +0000

    Basic text widget with all properties

---
+++ lib/TextWidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TextWidgetExample extends StatelessWidget {
+
+    const TextWidgetExample({super.key, ''});
+
+  Widget build(BuildContext context) {
+    Text(
+      'Hello, world!',
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: Colors.blue,
+        letterSpacing: 1.5,
+        wordSpacing: 5.0,
+        shadows: [
+          Shadow(
+            color: Colors.grey,
+            offset: Offset(2, 2),
+            blurRadius: 5.0,
+          ),
+        ],
+        decoration: TextDecoration.underline,
+        decorationColor: Colors.red,
+        decorationStyle: TextDecorationStyle.dashed,
+        backgroundColor: Colors.yellow,
+        height: 1.5,
+      ),
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 2,
+    );
+  }
+}
