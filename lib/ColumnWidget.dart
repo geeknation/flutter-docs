d9d392373117f2a81b9d5fb4c0ea871f053ee0c9
ian_mukunya2 <ianmu4046@gmail.com>
2023-03-18 08:30:36 +0000

    column widget

--- lib/ColumnWidget.dart
+++ lib/ColumnWidget.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ColumnWidget extends StatelessWidget {
-  Widget build(BuildContext contxt) {}
+  Widget build(BuildContext contxt) {
+    return (Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      textBaseline: TextBaseline.alphabetic,
+      textDirection: TextDirection.ltr,
+      verticalDirection: VerticalDirection.down,
+      children: [
+        Container(
+          width: 50.0,
+          height: 50.0,
+          color: Colors.red,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          width: 50.0,
+          height: 50.0,
+          color: Colors.green,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          width: 50.0,
+          height: 50.0,
+          color: Colors.blue,
+        ),
+      ],
+    ));
+  }
 }
