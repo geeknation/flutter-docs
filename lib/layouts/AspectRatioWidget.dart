6f2bb5ba9890f86299a94e3bd69878493ff70960
ian_mukunya2 <ianmu4046@gmail.com>
2023-03-19 06:12:44 +0000

    Aspect ratio widget

---
+++ lib/layouts/AspectRatioWidget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AspectRatioWidget extends StatelessWidget {
+  const AspectRatioWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: 100.0,
+      child: AspectRatio(
+        aspectRatio: 16 / 9,
+        child: Container(
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+}
